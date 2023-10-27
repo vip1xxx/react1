2cc277e083d217d560aeb07e66c80dcdeb757830
x <fedorenkomcbook@gmail.com>
2023-10-27 17:09:03 +0200

    pushing1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {Simpsons} from "./components/SimpsonContainer/Simpsons";
+import {Characters} from "./components/CharactersContainer/Characters";
+
+const App = () => {
+  return (
+      <div>
+        {/*<Simpsons/>*/}
+        <Characters/>
+      </div>
+  );
+};
+
+export {App};
